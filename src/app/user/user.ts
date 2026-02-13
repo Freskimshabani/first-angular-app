a8e81c5c4ffb00d711708fa477e3d1ed2010717e
Freskim Shabani <Freskimi2004@gmail.com>
2026-02-13 15:59:53 +0100

    Added Tasks Component Refactored Users Component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
+
+type UserObj = {
+  id: string;
+  profile: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -6,11 +12,26 @@ import { Component, Input } from '@angular/core';
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {
 
-  @Input() profile: string = '';
+export class User {
+  // @Input({required: true}) user!: {
+  //   id: string;
+  //   name: string;
+  //   profile: string;
+  // };
+  @Input({required: true}) user!: UserObj;
+
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) profile: string = 'user-1.jpg';
+  // @Input({required: true}) name: string = 'Username';
+
+  @Output() select = new EventEmitter<string>();
+
+  get userProfile(){
+    return 'assets/users/' + this.user.profile;
+  }
 
   onSelectUser() {
-
+    this.select.emit(this.user.id);
   }
 }
